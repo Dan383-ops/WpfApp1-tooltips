0eb8a3fc034077599d28e6fbdf6dcb6c4ad58908
Половьянов Данил <Danil_Polovianov@stud.vvsu.ru>
2024-12-10 17:20:07 +1000

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private List<string> imagesStore;
+		private int currentIndex = 0;
+		public MainWindow()
+		{
+			InitializeComponent();
+			LoadImages(@"\\sysprofiles.adm.vvsu.ru\STUDENTRPROFILES$\Danil_Polovianov\Downloads\DB");
+			DisplayImage();
+		}
+
+		private void LoadImages(string path)
+		{
+			if (Directory.Exists(path))
+			{
+				imagesStore = new List<string>(Directory.GetFiles((path), "*.jpg"));
+				imagesStore.AddRange(Directory.GetFiles((path), "*.png"));
+				imagesStore.AddRange(Directory.GetFiles((path), "*.bmp"));
+			}
+		}
+
+		private void DisplayImage()
+		{
+			if (imagesStore == null || imagesStore.Count == 0)
+			{
+				MessageBox.Show("Empty Folder");
+				return;
+			}
+			var ImagePath = imagesStore[currentIndex];
+			DisplayImageForm.Source = new BitmapImage(new Uri(ImagePath));
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Window1 window = new Window1();
+			window.Show();
+			this.Close();
+		}
+
+		private void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+		{
+			if (imagesStore == null || imagesStore.Count == 0)
+			{
+				MessageBox.Show("Nothing");
+				return;
+			}
+			currentIndex = (currentIndex - + imagesStore.Count) % imagesStore.Count;
+			DisplayImage();
+		}
+
+		private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+		{
+			if (imagesStore == null || imagesStore.Count == 0)
+			{
+				MessageBox.Show("Nothing");
+				return;
+			}
+			currentIndex = (currentIndex + 1) % imagesStore.Count;
+			DisplayImage();
+		}
+
+		private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+		{
+			Window1 window = new Window1();
+			window.Show();
+			this.Close();
+		}
+	}
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Логика взаимодействия для Window1.xaml
+	/// </summary>
+
+	public partial class Window1 : Window
+	{
+		private double _currentValue = 0;
+		private double _storedValue = 0;
+		private double _operation = '\0';
+		public Window1()
+		{
+			InitializeComponent();
+		}
+
+		private void NumberButton_Click(object sender, RoutedEventArgs e)
+		{
+			Button button = (Button)sender;
+			string number = button.Content.ToString();
+			if(txtDisplay.Text == "0" && number != ".")
+			{
+				txtDisplay.Text = number;
+			}
+			else
+			{
+				txtDisplay.Text += number;
+			}
+		}
+
+		private void OperationButton_Click(object sender, RoutedEventArgs e)
+		{
+			Button button = (Button)sender;
+			_operation = button.Content.ToString()[0];
+			_storedValue = double.Parse(txtDisplay.Text);
+			txtDisplay.Text = "0";
+		}
+
+		private void EqualsButton_Click(object sender, RoutedEventArgs e)
+		{
+			_currentValue = double.Parse(txtDisplay.Text);
+			switch (_operation)
+			{
+				case '+':
+					_currentValue = _storedValue + _currentValue;
+					break;
+				case '-':
+					_currentValue = _storedValue - _currentValue;
+					break;
+				case '*':
+					_currentValue = _currentValue * _storedValue;
+					break;
+				case '/':
+					if(_currentValue != 0)
+					{
+						_currentValue = _storedValue / _currentValue;
+					} else
+					{
+						MessageBox.Show("Що ты лысый? Обоюдно!");
+					}
+					break;
+			}
+			txtDisplay.Text = _currentValue.ToString();
+			_operation = '\0';
+		}
+
+		private void btn7_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn8_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn9_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn4_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn5_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn6_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn1_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn2_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn3_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btn0_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnDecimal_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnEquals_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnAdd_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnRemove_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnclear_Click(object sender, RoutedEventArgs e)
+		{
+			txtDisplay.Text = "0";
+			_currentValue = 0;
+			_operation = '\0';
+			_storedValue = 0;
+		}
+
+		private void btnMuti_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void btnDivis_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		/*private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			MainWindow window = new MainWindow();
+			window.Show();
+			this.Close();
+		}
+		private void MenuItem1_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			TextBox textBox = sender as TextBox;
+			MessageBox.Show(textBox.Text);
+		}
+
+		private void btn4_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	*/
+	}
+}
